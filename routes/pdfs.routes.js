57e5ccf0d9e5de3aab8225aee40c6f0948a18096
Eduard Albrecht <eduard.albrecht@edlixstudios.de>
2020-09-28 16:13:42 +0200

    add: cloud

--- routes/pdfs.routes.js
+++ routes/pdfs.routes.js
@@ -11,11 +11,11 @@ router.route("/:tool").get((req,res)=>{
     const searchName = req.params.tool
     const foundPdfs = pdfNames.find(e => e.match(searchName))
 
-    console.log("http://localhost:5000/assets/pdf/"+ foundPdfs)
+    console.log("http://192.168.178.54:5000/assets/pdf/"+ foundPdfs)
 
-    res.send("http://localhost:5000/assets/pdf/"+foundPdfs)
+    res.send("http://192.168.178.54:5000/assets/pdf/"+foundPdfs)
     
-
+    //http://assets.edlixstudios.de/pdf
 })
 
 
