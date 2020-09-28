935857a8de98a89ff45a787ef0a8933265ffced5
Eduard Albrecht <eduard.albrecht@edlixstudios.de>
2020-09-28 15:34:00 +0200

    add: backend

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use("/assets",express.static(path.join(__dirname,"assets")))
 const pdfs = require("./routes/pdfs.routes")
 app.use("/pdf",pdfs)
 
+app.get("/",(req,res)=>{
+    res.send("Server online")
+})
+
+
 app.listen(5000,()=>{
     console.log("Listen on port http://localhost:5000")
 })
