11cd7be87387cb4758fcd964b53984960f63ab87
Eduard <info@edlixstudios.de>
2020-09-29 09:59:24 +0200

    fast push

--- routes/pdfs.routes.js
+++ routes/pdfs.routes.js
@@ -2,19 +2,21 @@ const router = require("express").Router()
 const fs = require("fs")
 
 
-const pdfNames = fs.readdirSync(__dirname+"/pdf")
+const pdfNames = fs.readdirSync(__dirname + "/pdf")
 
 
 
-router.route("/:tool").get((req,res)=>{
+router.route("/:tool").get((req, res) => {
 
     const searchName = req.params.tool
     const foundPdfs = pdfNames.find(e => e.match(searchName))
 
-    console.log("http://192.168.178.54:5000/assets/pdf/"+ foundPdfs)
+    // console.log("http://192.168.178.54:5000/assets/pdf/"+ foundPdfs)
+    console.log("https://schneeberger-backend-qe9c481m3.vercel.app/assets/pdf/" + foundPdfs)
 
-    res.send("http://192.168.178.54:5000/assets/pdf/"+foundPdfs)
-    
+    //   res.send("http://192.168.178.54:5000/assets/pdf/"+foundPdfs)
+    res.send("https://schneeberger-backend-qe9c481m3.vercel.app/assets/pdf/" + foundPdfs)
+
     //http://assets.edlixstudios.de/pdf
 })
 
